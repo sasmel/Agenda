7ba04755d8a169ef4641f3a2fc2aae054810fed2
sasmel <s4muk4z@gmail.com>
2023-01-13 01:18:18 -0300

    Final Commit.

---
+++ app/src/main/java/br/com/alura/agenda/model/Aluno.java
@@ -0,0 +1,19 @@
+package br.com.alura.agenda.model;
+
+import androidx.annotation.NonNull;
+
+public class Aluno {
+    private final String nome;
+    private final String telefone;
+    private final String email;
+
+    public Aluno(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    @NonNull
+    @Override
+    public String toString() { return nome; }
+}
---
+++ app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -0,0 +1,55 @@
+package br.com.alura.agenda.ui.activity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import br.com.alura.agenda.DAO.AlunoDAO;
+import br.com.alura.agenda.R;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR="Lista de Alunos";
+    private final AlunoDAO dao = new AlunoDAO();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+        setTitle(TITULO_APPBAR);
+
+        configuraFabNovoAluno();
+
+    }
+
+    private void configuraFabNovoAluno(){
+        FloatingActionButton botaoNovoAluno = findViewById(R.id.activity_lista_alunos_fab_novo_aluno);
+        botaoNovoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abreFormularioAlunoActivity();
+            }
+        });
+    }
+
+    private void abreFormularioAlunoActivity(){
+        startActivity(new Intent(ListaAlunosActivity.this,
+                FormularioAlunoActivity.class));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        configuraLista();
+    }
+
+    private void configuraLista(){
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dao.todos()));
+    }
+}
