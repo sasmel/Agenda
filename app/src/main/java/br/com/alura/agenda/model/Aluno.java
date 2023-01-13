f422f1faf455414045edb8365eb78f70d3b3ab5f
Samuel Silva <samuel.soares@poscontrole.com.br>
2023-01-13 15:02:51 -0300

    Nessa atualização, adicionei o click nos nomes. Agora ao clicar nos nomes dos alunos nós podemos visualizar suas informações.

---
+++ app/src/main/java/br/com/alura/agenda/model/Aluno.java
@@ -0,0 +1,32 @@
+package br.com.alura.agenda.model;
+
+import androidx.annotation.NonNull;
+import java.io.Serializable;
+
+public class Aluno implements Serializable {
+    private final String nome;
+    private final String telefone;
+    private final String email;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Aluno(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    @NonNull
+    @Override
+    public String toString() { return nome; }
+}
---
+++ app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -0,0 +1,76 @@
+package br.com.alura.agenda.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
+
+import br.com.alura.agenda.DAO.AlunoDAO;
+import br.com.alura.agenda.R;
+import br.com.alura.agenda.model.Aluno;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR="Lista de Alunos";
+    private final AlunoDAO dao = new AlunoDAO();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+        setTitle(TITULO_APPBAR);
+        configuraFabNovoAluno();
+        dao.salva(new Aluno("Sam", "71988888888", "sam@sam.com"));
+        dao.salva(new Aluno("Sid", "75999999999", "sid@sid.com"));
+
+    }
+
+    private void configuraFabNovoAluno(){
+        FloatingActionButton botaoNovoAluno = findViewById(R.id.activity_lista_alunos_fab_novo_aluno);
+        botaoNovoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abreFormularioAlunoActivity();
+            }
+        });
+    }
+
+    private void abreFormularioAlunoActivity(){
+        startActivity(new Intent(ListaAlunosActivity.this,
+                FormularioAlunoActivity.class));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        configuraLista();
+    }
+
+    private void configuraLista(){
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
+        List<Aluno> alunos = dao.todos();
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                alunos));
+        listaDeAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int posicao, long id) {
+                Aluno alunoEscolhido = alunos.get(posicao);
+                Intent vaiParaFormularioActivity = new Intent(ListaAlunosActivity.this, FormularioAlunoActivity.class);
+                vaiParaFormularioActivity.putExtra("aluno", alunoEscolhido);
+                startActivity(vaiParaFormularioActivity);
+
+            }
+        });
+        };
+    }
